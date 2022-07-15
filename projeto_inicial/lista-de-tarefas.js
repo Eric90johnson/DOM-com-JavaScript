098edc45d2fd58770454881e6640d4b5220b72f1
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-07-14 22:18:16 -0300

    aplicando JS
    Melhorando a semântica do código

--- projeto_inicial/lista-de-tarefas.js
+++ projeto_inicial/lista-de-tarefas.js
@@ -1,6 +1,12 @@
-const novaTarefa = document.querySelector('[data-form-button]')
+const criarTarefa = (evento)=> {
 
-novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')})
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+    console.log(valor);
+    input.value = "";
+}
 
+const novaTarefa = document.querySelector('[data-form-button]');
 
- 
+novaTarefa.addEventListener('click', criarTarefa);
